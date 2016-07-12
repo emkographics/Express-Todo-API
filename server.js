5f121547b317e8c7ea79de29c1b2d08e0d90dffb
Memic <emir.memic@mercy.net>
2016-07-11 20:53:27 -0500

    Added post /todos

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3002;
+var todos = [];
+var todoNextId = 1;
+/*
 var todos = [{
 	id: 1,
 	description: 'Take car to the shop',
@@ -11,9 +15,12 @@ var todos = [{
 	completed: false,
 }, {
 	id: 3,
-	description: 'Complete Java course',
+	description: 'Complete Java programming course',
 	completed: true,
 }];
+*/
+
+app.use(bodyParser.json());
 
 // GET /
 app.get('/', function(req, res){
@@ -23,7 +30,7 @@ app.get('/', function(req, res){
 // GET /todos
 app.get('/todos', function(req, res){
 	res.json(todos);
-})
+});
 
 // GET/todos/:id (req.params.id)
 app.get('/todos/:id', function(req, res){
@@ -43,8 +50,22 @@ app.get('/todos/:id', function(req, res){
 	else {
 		res.status(404).send();
 	}
-})
+});
+
+// POST /todos
+app.post('/todos', function(req, res){
+	var body = req.body;
+
+	// add id to field
+	body.id = todoNextId;
+	todoNextId++;
+
+	// push body into array
+	todos.push(body);
+
+	res.json(body);
+});
 
 app.listen(PORT, function(){
 	console.log('Express listening on port: ' + PORT);
-})
+});
