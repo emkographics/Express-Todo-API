da96519cc772794114f11026fd490e8f42201ef4
Memic <emir.memic@mercy.net>
2016-07-16 23:59:14 -0500

    Add better authentication and refactor code

--- models/user.js
+++ models/user.js
@@ -1,20 +1,73 @@
-// required by sequelize.import
-module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('user', {
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true,
+        isEmail: true
       }
     },
+    salt: {
+      type: DataTypes.STRING
+    },
+    password_hash: {
+      type: DataTypes.STRING
+    },
     password: {
-      type: DataTypes.STRING,
+      type: DataTypes.VIRTUAL,
       allowNull: false,
       validate: {
         len: [7, 100]
+      },
+      set: function(value) {
+        var salt = bcrypt.genSaltSync(10);
+        var hashedPassword = bcrypt.hashSync(value, salt);
+
+        this.setDataValue('password', value);
+        this.setDataValue('salt', salt);
+        this.setDataValue('password_hash', hashedPassword);
+      }
+    }
+  }, {
+    hooks: {
+      beforeValidate: function(user, options) {
+        if (typeof user.email === 'string') {
+          user.email = user.email.toLowerCase();
+        }
+      }
+    },
+    classMethods: {
+      authenticate: function(body) {
+        return new Promise(function(resolve, reject) {
+          if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+            return reject.status(400).send();
+          }
+
+          user.findOne({
+            where: {
+              email: body.email
+            }
+          }).then(function(user) {
+            if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+              return reject()
+            }
+            resolve(user);
+          }, function(e) {
+            reject();
+          });
+        });
+      }
+    },
+    instanceMethods: {
+      toPublicJSON: function() {
+        var json = this.toJSON();
+        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
       }
     }
   });
+  return user;
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var bcrypt = require('bcrypt');
 
 // require data object
 var db = require('./db.js');
@@ -136,7 +137,7 @@ app.put('/todos/:id', function(req, res) {
   });
 });
 
-// POST /todos
+// POST /users
 app.post('/users', function(req, res) {
   // allow only defined properties
   var body = _.pick(req.body, 'email', 'password');
@@ -148,13 +149,24 @@ app.post('/users', function(req, res) {
   body.email = body.email.trim();
   // increment to next id
   db.user.create(body).then(function(user) {
-    res.json(user.toJSON());
+    res.json(user.toPublicJSON());
   }, function(e) {
     res.status(400).json(e);
   });
 });
 
-db.sequelize.sync().then(function() {
+//POST users/login
+app.post('/users/login', function(req, res) {
+  var body = _.pick(req.body, 'email', 'password');
+
+  db.user.authenticate(body).then(function (user){
+    res.json(user.toPublicJSON());
+  }, function () {
+      res.status(401).send();
+  });
+});
+
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log('Express listening on port: ' + PORT);
   });
